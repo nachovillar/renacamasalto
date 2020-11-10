4a7b6c7d993d69ac007bb86b2c133b246abb243b
pabloOnate <31405588+pabloOnate@users.noreply.github.com>
2020-11-10 01:07:49 -0300

    Se agrega olvidar contraseña

--- src/componentes/OlvideContrasena.js
+++ src/componentes/OlvideContrasena.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React,{useState} from 'react';
+import logo from '../imagenes/logo.png';
+import './OlvideContrasena.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap'
+import Footer from './footer/Footer'
+import { Link } from 'react-router-dom';
 
 const OlvideContrasena = () => {
-    return ( 
-        <h1>Desde Olvide Contraseña</h1>
 
+    const [usuario, setUsuario] = useState({
+        rut: '',
+    })
+
+    const {rut} = usuario;
+
+    const onChange = e => {
+        setUsuario({
+        ...usuario,
+        [e.target.getAttribute('name')]: e.target.value
+        })
+    }
+
+    return ( 
+        <div className = "OlvideContrasena">
+            
+            <div>
+                <img src = {logo} alt = "Imagen del logo de reñaca más alto" className = "logo" />
+            </div>
+
+            <Form>
+                <div className = "contenedorInputs">
+                    
+                    <div className = "inputbox">
+                        <h2 id="tituloRest">Restablecer Contraseña</h2>
+                        <Form.Group>
+                            <Form.Label>INGRESE SU RUT</Form.Label>
+                            <Form.Control 
+                                name = "rut"
+                                type = "text" 
+                                placeholder = "Ej: 12345678-9" 
+                                value = {rut}
+                                onChange = {onChange}
+                                    
+                            />
+                        </Form.Group>
+                    </div>
+
+                    <div className = "submitButton">
+                        <Button
+                                variant = "success"
+                                type = "submit"
+                                className = "button-primary"
+                                
+                        >Enviar</Button>
+                    
+                    </div>
+                    <div className = "Inicio">
+                        <Link to = "/" className = "enlace-login">
+                            Volver al inicio de sesión
+                        </Link>
+                    </div>
+                </div>
+            </Form>
+            <div>
+                <Footer></Footer>
+            </div>
+        </div>
      );
 }
  
