053e034791463e876737aef290a6dd68ea001127
José Ignacio Villar <jose.villar.12@sansano.usm.cl>
2020-11-16 13:00:42 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css'
 import React from 'react'
 import Login from './componentes/auth/Login'
 import OlvideContrasena from './componentes/OlvideContrasena'
-<<<<<<< HEAD
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-=======
->>>>>>> 4547ee75fb0e51bf44a8107c564be8d900010332
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PerfilAdministrador from './componentes/Perfiles/administrador/PerfilAdministrador'
 import PerfilVoluntario from './componentes/Perfiles/voluntario/PerfilVoluntario'
@@ -17,8 +14,7 @@ import ActividadState from './context/actividad/ActividadState'
 function App() {
 
   return (
-<<<<<<< HEAD
-    
+  <ActividadState>
     <Router>
       <Switch>
         <Route exact path = "/" component = {Login}    />
@@ -27,19 +23,8 @@ function App() {
         <Route exact path = "/pefil-voluntario" component = {PerfilVoluntario} />
       </Switch>
     </Router>
+  </ActividadState>
     
-=======
-    <ActividadState>
-      <Router>
-        <Switch>
-          <Route exact path = "/" component = {Login}    />
-          <Route exact path = "/olvide-contrasena" component = {OlvideContrasena} />
-          <Route exact path = "/perfil-admin" component = {PerfilAdministrador} />
-          <Route exact path = "/pefil-voluntario" component = {PerfilVoluntario} />
-        </Switch>
-      </Router>
-    </ActividadState>
->>>>>>> 4547ee75fb0e51bf44a8107c564be8d900010332
   );
 }
 
