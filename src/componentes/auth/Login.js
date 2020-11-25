4a304d77c77b9577a2e73edbe810446ca33c3119
José Ignacio Villar <jose.villar.12@sansano.usm.cl>
2020-11-24 23:12:38 -0300

    ñe
    ñe

--- src/componentes/auth/Login.js
+++ src/componentes/auth/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
         if(autenticado){
             props.history.push("/perfil-admin")
         }
-
+        
         if(mensaje) {
             mostrarAlerta(mensaje.msg, mensaje.categoria)
         }
